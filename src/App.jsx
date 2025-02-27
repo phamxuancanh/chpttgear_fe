ffaf65a865b71bc366cd7d24e01469db311c0ca6
phamxuancanh <114747055+phamxuancanh@users.noreply.github.com>
2025-02-27 16:14:38 +0700

    navbar gui & home

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,31 @@ import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Helmet } from "react-helmet";
-import { ToastContainer, toast, Bounce } from "react-toastify";
+import { ToastContainer, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRouter from "./routers/AppRouter";
 import { store, persistor } from "./redux/store";
-import { reload } from "./utils/functions"
-
+import { reload } from "./utils/functions";
+import { CategoryProvider } from "./context/CategoryContext";
 import ErrorBoundary from "./components/ErrorBoundary";
+
 const queryClient = new QueryClient();
+
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <QueryClientProvider client={queryClient}>
-          <Helmet>
-            <title>CHPTT GEAR</title>
-            <link rel="shortcut icon" href="./logo192.png" />
-          </Helmet>
-          <ErrorBoundary onReset={reload}>
-            <AppRouter />
-          </ErrorBoundary>
+          <CategoryProvider>  {/* Bọc ở đây */}
+            <Helmet>
+              <title>CHPTT GEAR</title>
+              <link rel="shortcut icon" href="./logo192.png" />
+            </Helmet>
+            <ErrorBoundary onReset={reload}>
+              <AppRouter />
+            </ErrorBoundary>
+          </CategoryProvider>
           <ToastContainer
             position="bottom-right"
             autoClose={2000}
